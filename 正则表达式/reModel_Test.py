a478cc6e4d741f56c8ba894110f19023b01851c2
hopeonme@126.com <hopeonme@126.com>
2019-09-10 16:28:49 +0800

    re测试

--- 正则表达式/reModel_Test.py
+++ 正则表达式/reModel_Test.py
@@ -2,13 +2,15 @@
 import re
 
 def test1():
-    content = '(az/rb/.*?.jpg)(az/hprichbg/rb/.*?.jpg)(az/hprichbg/rb/.*?.jpg)'
-    reg = r'(az/hprichbg/rb/.*?.jpg)'
+    content = '(az/hprichbg/rb/jpg) az/hprichbg/rb/..jpg az/hprichbg/r/.jpg az/hprichbg/rb/  vv...jpg)'
+    content1 = '(a)a)'
+    reg = r'(az/hprichbg/rb/.*?\.jpg)'
+    reg1 = r'a'
+    print(content)
     a = re.findall(reg, content, re.S)
     print(a)
+    print(re.S)
 
 
 if __name__ == '__main__':
-    test1()
-    test1()
     test1()
